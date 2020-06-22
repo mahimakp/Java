c86d2922c7d0a9bb9875e8cbf02ec77bcc1ab152
mahimakp <37635822+mahimakp@users.noreply.github.com>
2020-06-22 17:56:34 +0530

    Add files via upload

---
+++ Seasons.java
@@ -0,0 +1,50 @@
+class Seasons{
+
+	public static void main(String[] args){
+
+		int month = Integer.parseInt( args[0] );
+
+		switch(month){
+
+			case 1: System.out.println(" Winter ");
+					break;
+			
+			case 2: System.out.println(" Winter ");
+					break;
+			
+			case 3:  System.out.println(" Spring ");
+					break;
+			
+			case 4:  System.out.println(" Spring ");
+					break;
+			
+			case 5:  System.out.println(" Spring ");
+					break;
+			
+			case 6:  System.out.println(" Summer ");
+					break;
+			
+			case 7:  System.out.println(" Summer ");
+					break;
+			
+			case 8:  System.out.println(" Summer ");
+					break;
+			
+			case 9:  System.out.println(" Autumn ");
+					break;
+			
+			case 10:  System.out.println(" Autumn ");
+					break;
+			
+			case 11:  System.out.println(" Autumn ");
+					break;
+			
+			case 12:  System.out.println(" Winter ");
+					break;
+
+			default : System.out.println("No such Season exists");
+			 
+		}
+
+	}
+}
